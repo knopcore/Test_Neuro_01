e310fb22e326fcf94a88198ea4672cbaad3d1ba0
knopcore <82874917+knopcore@users.noreply.github.com>
2021-10-30 20:45:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Test_Neuro_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(104, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(28, 28);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(13, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 380);
+            this.textBox1.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(-5, 58);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(633, 645);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(685, 575);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(685, 575);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(629, 557);
+            this.textBox2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.panel2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(625, 616);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(26, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(560, 560);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 702);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Neuro_01
+{
+    public partial class Form1 : Form
+    {
+        Color CurrentColor = Color.Black; //Brushes.DeepSkyBlue
+        bool IsPressed = false;
+        Point CurrentPoint;
+        Point PrevPoint;
+        Graphics g1;
+
+        //массивы значений
+        int[,] jaggedArray1 = new int[28, 28];
+        float[,] jaggedArray2 = new float[28, 28];
+       
+        // графика на панели 2, крупные квадраты
+        Graphics g2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g1 = panel1.CreateGraphics();
+            g2 = panel2.CreateGraphics();
+
+
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsPressed = true;
+            CurrentPoint = e.Location;
+
+
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsPressed = false;
+ 
+            textBox1.Clear();
+            string s1 = "";
+            string s2 = "";
+
+            string f1 = "";
+            string f2 = "";
+
+            for (int i = 0; i < 28; i++)
+            {
+                s2 = "";
+                f2 = "";
+                for (int j = 0; j < 28; j++)
+                {
+                    s2 = s2 + (jaggedArray1[j, i]) + " ";
+                    f2 = f2 + $"{jaggedArray2[j, i],4}"  + " ";
+                    
+        }
+                s1 = s1 + s2 + Environment.NewLine;
+                f1 = f1 + f2 + Environment.NewLine;
+            }
+            textBox1.Text = s1;
+            textBox2.Text = f1;
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsPressed)
+            {
+                PrevPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+
+                for_paint();
+
+                if ((e.X < 28)&&(e.Y < 28)&&(e.X > 0) && (e.Y > 0))
+                {
+                    //Записать в масссив целых
+                    jaggedArray1[e.X, e.Y] = 1;
+
+                    //Записать в масссив с точкой
+                    for (int i = 0; i < 28; i++)
+                    {
+                        for (int j = 0; j < 28; j++)
+                        {
+                            if ( (e.X != i) && (e.Y != j) )
+                            {
+                                float tst = 0.8f / ((i - e.X) * (i - e.X) + (j - e.Y) * (j - e.Y));
+                                if (tst > 0.1f)
+                                {
+                                     jaggedArray2[i, j] = tst;
+                                }
+
+                            }
+
+                            if ((e.X == i) && (e.Y == j))
+                            {
+                                jaggedArray2[i, j] = 1.0f;
+                            }
+                            
+                        }
+                    }
+   
+
+                }
+
+
+
+            }
+        }
+
+
+        private void for_paint()
+        {
+            Pen blackPen = new Pen(CurrentColor);
+            g1.DrawLine(blackPen, PrevPoint, CurrentPoint);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+            textBox1.Clear();
+            textBox2.Clear();
+            
+
+            for (int i = 0; i < 28; i++)
+            {
+                for (int j = 0; j < 28; j++)
+                {
+                    jaggedArray1[i, j] = 0;
+                    jaggedArray2[i, j] = 0;
+                }
+            }
+
+        }
+
+
+    }
+}
